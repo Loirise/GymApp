bcd074bb9b2888d5250ccd682ae4b299d0b3c66a
Loirise <wieczorekszymon4@gmail.com>
2023-03-22 19:22:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Benefits from "./components/Benefits";
+import OurClasses from "./components/OurClasses";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="app h-screen w-full bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Link.tsx
@@ -0,0 +1,27 @@
+import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage
+          ? "text-primary-500 underline underline-offset-8"
+          : ""
+      } transition duration-300 hover:text-primary-300 hover:underline hover:underline-offset-8`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
